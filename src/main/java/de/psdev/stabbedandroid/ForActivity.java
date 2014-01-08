1bb92ef8abec2ed938cc01d9e4eb395503f81999
Philip Schiffer <philip.schiffer@gmail.com>
2014-01-08 17:12:02 +0100

    Add @Documented and @Target to Annotations

--- src/main/java/de/psdev/stabbedandroid/ForActivity.java
+++ src/main/java/de/psdev/stabbedandroid/ForActivity.java
@@ -16,14 +16,24 @@
 
 package de.psdev.stabbedandroid;
 
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
 import javax.inject.Qualifier;
+
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Qualifier annotation to explicitly differentiate dependencies between application and activity context.
  */
 @Qualifier
+@Target({ METHOD, CONSTRUCTOR, FIELD, PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface ForActivity {
 }
